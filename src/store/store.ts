c7a283680b8fe50b449dbddf5501789ac5470417
Artur <metallist15431543@gmail.com>
2026-01-28 14:12:02 +0300

    feat: create store and post slice

---
+++ src/store/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { postApi } from "./post-slice";
+
+export const store = configureStore({
+  reducer: {
+    [postApi.reducerPath]: postApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(postApi.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
