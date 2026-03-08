ed307c6f04872c9f312a2698d51c69bdbdcd368f
Artur <metallist15431543@gmail.com>
2026-03-08 17:39:50 +0300

    feat: add user slice to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { baseApi } from "@/shared/api/baseApi";
 import { postsSlice } from "@/features/posts/model/postsSlice";
+import { usersSlice } from "@/features/users/model/userSlice";
 
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
     [postsSlice.name]: postsSlice.reducer,
+    [usersSlice.name]: usersSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
 });
